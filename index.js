970613640de8cf447909417bfc1e52b9996096a5
kjellmorten <post@kjellmorten.no>
2016-02-19 22:46:07 +0100

    Use mock in test environment

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 const couchdb = require('./lib/couchdb');
 
-module.exports = {
-  couchdb: couchdb
-};
+// Use mock module in test environment if dbdbmock is installed
+let useMock = (process.env.NODE_ENV === 'test');
+try {
+  if (useMock) {
+    require.resolve('dbdbmock');
+  }
+} catch(e) {
+  useMock = false;
+}
+
+if (useMock) {
+  // Export test module
+  module.exports = require('dbdbmock');
+} else {
+  // Export production module
+  module.exports = {couchdb: couchdb};
+}
